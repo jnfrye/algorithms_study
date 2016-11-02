33c2c1645bf126235c3fac479be2b05705f2f581
Josh Frye <jfrye@uoregon.edu>
2016-11-02 11:33:30 -0700

    Initial commit

---
+++ include/sorting.h
@@ -0,0 +1,8 @@
+//
+// Created by jfrye on 11/2/16.
+//
+
+#ifndef ALGORITHMS_STUDY_CPP_SORTING_H
+#define ALGORITHMS_STUDY_CPP_SORTING_H
+
+#endif //ALGORITHMS_STUDY_CPP_SORTING_H
---
+++ src/sorting.cpp
@@ -0,0 +1,7 @@
+//
+// Created by jfrye on 11/2/16.
+//
+
+#include "sorting.h"
+
+void hello
