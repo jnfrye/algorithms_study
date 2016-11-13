eee756c68dfcd9ee4cc848a66cd1b583702145e0
Josh Frye <jfrye@uoregon.edu>
2016-11-13 13:22:07 -0800

    Test linear search; add searching tests to CMake

--- test/searching_test.cpp
+++ test/searching_test.cpp
@@ -1,5 +1,35 @@
 /** Unit tests for `searching.cpp`
  */
 
-#include "searching.h"
+#include <string>       // std::string
+#include <vector>       // std::vector
+#include <algorithm>    // std::find
+
 #include "gtest/gtest.h"
+
+#include "searching.h"
+
+
+class GeneralSearchingTest: public ::testing::Test {
+public:
+    std::vector<int> singleton;
+    std::vector<int> vec;
+
+protected:
+    virtual void SetUp() {
+        singleton = {5};
+        vec = {5, 3, 6, 2, 8, 3, 1};
+    }
+};
+
+TEST_F(GeneralSearchingTest, SearchingFindsKnownItem) {
+    int index = LinearSearch(vec, 2);
+
+    // We can also find the index using the std::find function
+    auto iterator = std::find(vec.begin(), vec.end(), 2);
+    // convert from iterator to integer index
+    int comparison_index = std::distance(vec.begin(), iterator);
+
+    EXPECT_EQ(index, 3);
+    EXPECT_EQ(index, comparison_index);
+}
