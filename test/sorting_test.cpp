fd4be7b6a5e72719910655a5c6fbf32c22906193
Josh Frye <jfrye@uoregon.edu>
2016-11-13 12:18:31 -0800

    Add another test of InsertionSort; rename other test

--- test/sorting_test.cpp
+++ test/sorting_test.cpp
@@ -7,11 +7,21 @@
 
 #include "sorting.h"
 
-TEST(InsertionSortTest, Size1Vector) {
+
+TEST(InsertionSortTest, PreservesSize1Vector) {
     std::vector<int> singleton = {1};
     std::vector<int> original_singleton(singleton);
     InsertionSort(singleton);
 
     EXPECT_EQ(singleton, original_singleton) <<
         "A singleton should not change when sorted";
+}
+
+TEST(InsertionSortTest, CorrectlySortsKnownVector) {
+    std::vector<int> vec = {5, 3, 6, 2, 8, 3, 1};
+    std::vector<int> expected_vec = {1, 2, 3, 3, 5, 6, 8};
+    InsertionSort(vec);
+
+    EXPECT_EQ(vec, expected_vec) <<
+        "Sorted vector did not match with vector sorted by hand!";
 }
