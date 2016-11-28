0d00ebc1acf8cf23426981e7ccbc2bd57c60186b
Josh Frye <jfrye@uoregon.edu>
2016-11-28 11:07:52 -0800

    Move matrix typedefs to header

--- include/algorithm/matrix/matrix.hpp
+++ include/algorithm/matrix/matrix.hpp
@@ -4,6 +4,9 @@
 #ifndef ALGORITHMS_STUDY_CPP_MATRIX_HPP
 #define ALGORITHMS_STUDY_CPP_MATRIX_HPP
 
+#include <vector>
 
+typedef std::vector<int> Row;
+typedef std::vector<Row> Matrix;
 
 #endif //ALGORITHMS_STUDY_CPP_MATRIX_HPP
