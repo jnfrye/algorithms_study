04b30416f61343fba2f2c5cf0ca8e2a671006a3c
Josh Frye <jfrye@uoregon.edu>
2016-11-29 12:24:09 -0800

    Code initial version of random_interval(...) function

--- include/algorithm/random.hpp
+++ include/algorithm/random.hpp
@@ -1,7 +1,19 @@
 /** Convenience functions for random numbers.
+ *
+ * All functions assume that the random seed (std::srand) has been set!
  */
 
 #ifndef ALGORITHMS_STUDY_CPP_RANDOM_HPP
 #define ALGORITHMS_STUDY_CPP_RANDOM_HPP
+
+/** Generate a random integer in an inclusive interval.
+ *
+ * That is, returned integer could be `min`, `max`, or any integer between.
+ *
+ * @param min   (Inclusive) lower bound for random range.
+ * @param max   (Inclusive) upper bound for random range.
+ * @return      Random number in desired range.
+ */
+int random_interval(const int min, const int max);
 
 #endif //ALGORITHMS_STUDY_CPP_RANDOM_HPP
--- src/random.cpp
+++ src/random.cpp
@@ -1,1 +1,13 @@
 #include "algorithm/random.hpp"
+
+#include <random>
+
+
+std::random_device rng_seed;     // only used once to initialise (seed) engine
+std::mt19937 rng(rng_seed());    // random-number engine used (Mersenne-Twister in this case)
+
+
+int random_interval(const int min, const int max) {
+    std::uniform_int_distribution<int> random_distribution(min, max);
+    return random_distribution(rng);
+}
