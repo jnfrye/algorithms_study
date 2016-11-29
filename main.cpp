5e7cfdc8b91dfdda8a5959b9ef5d72249c4b55df
Josh Frye <jfrye@uoregon.edu>
2016-11-29 13:04:22 -0800

    Demonstrate the random matrix filling function

--- main.cpp
+++ main.cpp
@@ -6,23 +6,27 @@
 #include "algorithm/vector/sort.hpp"
 #include "algorithm/vector/search.hpp"
 #include "algorithm/matrix/matrix.hpp"
+#include "algorithm/random.hpp"
 
 
 int main() {
 
-    Matrix A = {{1, 2},
-                {3, 4}};
+    Matrix A = Matrix(2, std::vector<int>(2, 0));
+    RandomlyFillMatrix(A, -5, 5);
     PrintMatrix(A);
     std::cout << std::endl;
 
-    Matrix B = {{5, 6},
-                {7, 8}};
+    Matrix B = Matrix(2, std::vector<int>(2, 0));
+    RandomlyFillMatrix(B, -5, 5);
     PrintMatrix(B);
     std::cout << std::endl;
 
     Matrix C = MatrixMultiplyBF(A, B);
 
     PrintMatrix(C);
+
+    for (int i = 0; i < 20; ++i)
+        std::cout << RandomInterval(0, 15) << ' ';
 
     return 0;
 }
